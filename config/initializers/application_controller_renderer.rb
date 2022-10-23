cc2e6c2d5198e8c17f82e3063c796d5df73f957b
SunadaAtaru <sleepingdoll2008@gmail.com>
2022-10-23 22:12:18 +0900

    ロゴ作成手前

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '8ee9c714ad0e60fcdf94295db7abebc8119ca0230412d225b9e1b4e7cf4dfe21403f7b27568de5af16f0ed7add5acca11afa134de221e425a2da608f379835d9'
