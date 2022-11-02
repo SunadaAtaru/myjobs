4ad40cfefeffe93110c3e38137b6b539ed0dc366
SunadaAtaru <sleepingdoll2008@gmail.com>
2022-11-02 23:28:06 +0900

    フォロー・フォロワー機能,中間テーブル

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only:[:index, :show, :edit, :update]  #この行を追加
-  
+  resources :users, only:[:index, :show, :edit, :update] do
+    member do
+      get :follows, :followers
+    end
+    resource :relationships, only: [:create, :destroy] 
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'homes#top'   
   resources :posts do
